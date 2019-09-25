7cbdafe9f34507e9a3956f158e61d143a769fbed
JosephLudena <55619385+JosephLudena@users.noreply.github.com>
2019-09-25 16:41:21 +0300

    Delete hw_2.py

--- hw_2.py
+++
@@ -1,7 +0,0 @@
-print("Вычисление площади треугольника по формуле Герона")
-a=float(input("сторона a "))
-b=float(input("сторона b "))
-c=float(input("сторона c "))
-s=float(a+b+c)/2
-area=float(s*(s-a)*(s-b)*(s-c))**(1/2)
-print("площад:", str(area))
